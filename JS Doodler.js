87d2014a1891eb5f24edc8554010a7704be3b56d
James Molzer <jamesmolzer@gmail.com>
2021-03-15 15:37:52 -0400

    this is the update

---
+++ JS Doodler.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    const grid = document.querySelector('.grid')
+    const Dood= document.createElement('div')
+    function createDood(){
+        grid.appendChild(Dood)
+        Dood.classList.add('dood')
+    }
+    
+})
+
